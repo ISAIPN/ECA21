6c4a9fed7114617a32e143a500c688d23b9e009b
ISAIPN <isa.ipn.zacatenco@outlook.com>
2021-08-11 03:09:24 -0500

    repo

---
+++ script.js
@@ -0,0 +1,40 @@
+function scrollToElement(element){
+  window.scrollTo({
+    'behavior': 'smooth',
+    'top': element.offsetTop,
+  });
+}
+
+document.querySelector(".menu")
+  .addEventListener('click',function(){
+    document.querySelector('.menu-screen').classList.add('active');
+  });
+
+  document.querySelector(".close")
+    .addEventListener('click',function(){
+      document.querySelector('.menu-screen').classList.remove('active');
+    })
+
+  let links = document.querySelectorAll(".menu-screen a");
+
+  // window.scrollTo({
+  //   'behavior': 'smooth',
+  //   'top': 1000,
+  // });
+
+  links.forEach(link =>{
+    link.addEventListener('click',function(ev){
+      document.querySelector('.menu-screen').classList.remove('active');
+
+      let paths = this.href.split("/");
+      const selector = paths[paths.length-1]
+
+      if(window.scrollTo) ev.preventDefault()
+
+      // console.log(selector);
+      scrollToElement(document.querySelector(selector));
+
+      return !!window.scrollTo;
+
+    })
+  })
